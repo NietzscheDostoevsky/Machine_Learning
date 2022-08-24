6e08f5cae3d8d977a53b702730af3fe37c6aedc2
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-08-24 15:37:14 +0530

    first draft full code

--- Mathematics_for_Machine_Learning_Specialization/C1_Linear_Algebra/Week_3.py
+++ Mathematics_for_Machine_Learning_Specialization/C1_Linear_Algebra/Week_3.py
@@ -83,8 +83,10 @@ def fixRowTwo(A):
     # Next we'll test that the diagonal element is not zero.
     if A[2, 2] == 0:
         # Insert code below that adds a lower row to row 2.
-
+        A[2] = A[2] + A[3]
         # Now repeat your code which sets the sub-diagonal elements to zero.
+        A[2] = A[2] - A[2, 0] * A[0]
+        A[2] = A[2] - A[2, 1] * A[1]
 
     if A[2, 2] == 0:
         raise MatrixIsSingular()
@@ -98,9 +100,12 @@ def fixRowTwo(A):
 
 def fixRowThree(A):
     # Insert code below to set the sub-diagonal elements of row three to zero.
+    A[3] = A[3] - A[3, 0] * A[0]  # this makes a30 = 0
+    A[3] = A[3] - A[3, 1] * A[1]   # this makes a31 = 0
+    A[3] = A[3] - A[3, 2] * A[2]   # this makes a32 = 0
 
     # Complete the if statement to test if the diagonal element is zero.
-    if:
+    if A[3, 3] == 0:
         raise MatrixIsSingular()
     # Transform the row to set the diagonal element to one.
 
