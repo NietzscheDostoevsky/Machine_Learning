7069852d9ba90371d36b358ad48c2774a911409b
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-08-28 22:23:50 +0530

    added generalized function

--- Mathematics_for_Machine_Learning_Specialization/C1_Linear_Algebra/Week_3_GSOrtho.py
+++ Mathematics_for_Machine_Learning_Specialization/C1_Linear_Algebra/Week_3_GSOrtho.py
@@ -69,6 +69,28 @@ def gsBasis4(A):
 # We'll use a for-loop here to iterate the process for each vector.
 
 
+def gsBasis(A):
+    # Make B as a copy of A, since we're going to alter it's values.
+    B = np.array(A, dtype=np.float_)
+    # Loop over all vectors, starting with zero, label them with i
+    for i in range(B.shape[1]):  # B.shape[1] gives the no of columns
+        # Inside that loop, loop over all previous vectors, j, to subtract.
+        for j in range(i):
+            # Complete the code to subtract the overlap with previous vectors.
+            # you'll need the current vector B[:, i] and a previous vector B[:, j]
+            B[:, i] = B[:, i] - B[:, i]   @  B[:, j] * B[:, j]
+            # B[:, 1] = B[:, 1] - B[:, 1] @ B[:, 0] * B[:, 0]
+        # Next insert code to do the normalisation test for B[:, i]
+        if la.norm(B[:, i]) > verySmallNumber:
+            B[:, i] = B[:, i] / la.norm(B[:, i])
+
+        else:
+            B[:, i] = np.zeros_like(B[:, i])
+
+            # Finally, we return the result:
+    return B
+
+
 # This function uses the Gram-schmidt process to calculate the dimension
 # spanned by a list of vectors.
 # Since each vector is normalised to one, or is zero,
