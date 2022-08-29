a3eb337525f88c4b9f504e0457a026b29ea88c13
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-08-29 07:32:42 +0530

    added packages

---
+++ Mathematics_for_Machine_Learning_Specialization/C1_Linear_Algebra/Week_3_Reflection_bear.py
@@ -0,0 +1,10 @@
+# %%
+# PACKAGE
+# Run this cell first once to load the dependancies.
+import numpy as np
+from numpy.linalg import norm, inv
+from numpy import transpose
+from readonly.bearNecessities import *
+
+# %%
+˝
