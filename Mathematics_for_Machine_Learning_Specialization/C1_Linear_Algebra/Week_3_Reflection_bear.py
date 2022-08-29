735a2d47b2dbf90e63bf83f1dff39417cb3a1b9c
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-08-29 07:33:10 +0530

    added body

--- Mathematics_for_Machine_Learning_Specialization/C1_Linear_Algebra/Week_3_Reflection_bear.py
+++ Mathematics_for_Machine_Learning_Specialization/C1_Linear_Algebra/Week_3_Reflection_bear.py
@@ -7,4 +7,28 @@ from numpy import transpose
 from readonly.bearNecessities import *
 
 # %%
+
+# GRADED FUNCTION
+# You should edit this cell.
+
+# In this function, you will return the transformation matrix T,
+# having built it out of an orthonormal basis set E that you create from Bear's Basis
+# and a transformation matrix in the mirror's coordinates TE.
+
+
+# The parameter bearBasis is a 2×2 matrix that is passed to the function.
+def build_reflection_matrix(bearBasis):
+    # Use the gsBasis function on bearBasis to get the mirror's orthonormal basis.
+    E = ???
+    # Write a matrix in component form that performs the mirror's reflection in the mirror's basis.
+    # Recall, the mirror operates by negating the last component of a vector.
+    # Replace a,b,c,d with appropriate values
+    TE = np.array([[a, b],
+                   [c, d]])
+    # Combine the matrices E and TE to produce your transformation matrix.
+    T = ???
+    # Finally, we return the result. There is no need to change this line.
+    return T
+
+
 ˝
