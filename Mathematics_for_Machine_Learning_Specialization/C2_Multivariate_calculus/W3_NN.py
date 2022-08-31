05a3879e5b46c6b50854fc6096bec8e6c252a21c
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-08-31 20:53:26 +0530

    ques 1 exercise NN

--- Mathematics_for_Machine_Learning_Specialization/C2_Multivariate_calculus/W3_NN.py
+++ Mathematics_for_Machine_Learning_Specialization/C2_Multivariate_calculus/W3_NN.py
@@ -9,10 +9,19 @@ b1 = -0.1
 # Then we define the neuron activation.
 
 
-def a1(a0):
+def a1(a0, w1, b1):
     return σ(w1 * a0 + b1)
 
 
 # Finally let's try the network out!
 # Replace x with 0 or 1 below,
-a1(0)
+w_b = [(10, 0), (0, 5), (3, 1), (-3, 0), (-5, 5)]
+
+for w, b in w_b:
+    print(w, b)
+    for i in range(2):
+        print("a1 : ", i)
+        print("w : ", w, "b : ", b)
+        print(a1(i, w, b))
+        print("-----------")
+        print("\n")
