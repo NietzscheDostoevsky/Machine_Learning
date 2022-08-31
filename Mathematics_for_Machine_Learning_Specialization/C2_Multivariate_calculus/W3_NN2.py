d161c124b7564182810566e2b7702bc2fb5652c1
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-08-31 22:00:49 +0530

    added note

--- Mathematics_for_Machine_Learning_Specialization/C2_Multivariate_calculus/W3_NN2.py
+++ Mathematics_for_Machine_Learning_Specialization/C2_Multivariate_calculus/W3_NN2.py
@@ -14,6 +14,6 @@ x = np.array([0.3, 0.4, 0.1])
 # and replace a1_0 and a1_1 here (to 2 decimal places)
 # (Or if you feel adventurous, find the values with code!)
 
-a1 = sigma((W @ x) + b)
+a1 = sigma((W @ x) + b)  # make a note of how to use input in a function
 
 print(a1)
