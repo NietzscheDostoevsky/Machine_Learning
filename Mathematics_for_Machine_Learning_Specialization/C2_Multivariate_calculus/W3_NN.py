72a09055ae6bd13602c186bcf39320ca9b17b133
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-08-31 20:41:38 +0530

    added code

---
+++ Mathematics_for_Machine_Learning_Specialization/C2_Multivariate_calculus/W3_NN.py
@@ -0,0 +1,17 @@
+# Simple Artificial Neural Networks _ Coursera
+
+# First we set the state of the network
+σ = np.tanh
+w1 = 1.3
+b1 = -0.1
+
+# Then we define the neuron activation.
+
+
+def a1(a0):
+    return σ(w1 * a0 + b1)
+
+
+# Finally let's try the network out!
+# Replace x with 0 or 1 below,
+a1(0)
