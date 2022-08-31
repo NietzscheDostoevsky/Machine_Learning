bc8c2565c1977300ca0b1b5c0a3234ea7432141b
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-08-31 21:22:08 +0530

    enter code

---
+++ Mathematics_for_Machine_Learning_Specialization/C2_Multivariate_calculus/W3_NN2.py
@@ -0,0 +1,14 @@
+# Ques 2 : https://www.coursera.org/learn/multivariate-calculus-machine-learning/quiz/vV8Og/simple-artificial-neural-networks/attempt
+
+# First set up the network.
+sigma = np.tanh
+W = np.array([[-2, 4, -1], [6, 0, -3]])
+b = np.array([0.1, -2.5])
+
+# Define our input vector
+x = np.array([0.3, 0.4, 0.1])
+
+# Calculate the values by hand,
+# and replace a1_0 and a1_1 here (to 2 decimal places)
+# (Or if you feel adventurous, find the values with code!)
+a1 = np.array([a1_0, a1_1])
