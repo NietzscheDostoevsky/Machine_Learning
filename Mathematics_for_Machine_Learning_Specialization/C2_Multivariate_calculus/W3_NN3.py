e4b256c030a70ae326c29b8789d7c65a3a0c7923
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-09-02 14:50:56 +0530

    update code

--- Mathematics_for_Machine_Learning_Specialization/C2_Multivariate_calculus/W3_NN3.py
+++ Mathematics_for_Machine_Learning_Specialization/C2_Multivariate_calculus/W3_NN3.py
@@ -1,4 +1,12 @@
-# First we set the state of the network σ = np.tanh w1 = 1.3 b1 = -0.1 # Then we define the neuron activation.
+# First we set the state of the network
+
+import numpy as np
+
+σ = np.tanh
+w1 = 1.3
+b1 = -0.1
+
+# Then we define the neuron activation.
 
 
 def a1(a0):
