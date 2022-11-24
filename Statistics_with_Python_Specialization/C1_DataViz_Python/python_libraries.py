035540096b4a9630aadd4b815ddc1da001dc7775
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-11-25 00:33:10 +0530

    Update python_libraries.py

--- Statistics_with_Python_Specialization/C1_DataViz_Python/python_libraries.py
+++ Statistics_with_Python_Specialization/C1_DataViz_Python/python_libraries.py
@@ -2,94 +2,99 @@
 # coding: utf-8
 
 # # Python Libraries
-# 
+#
 # For this tutorial, we are going to outline the most common uses for each of the following libraries:
-# 
+#
 # * **Numpy** is a library for working with arrays of data.
-# 
+#
 # * **Scipy** is a library of techniques for numerical and scientific computing.
-# 
+#
 # * **Matplotlib** is a library for making visualizations.
-# 
+#
 # * **Seaborn** is a higher-level interface to Matplotlib that can be used to simplify many visualization tasks.
-# 
+#
 # *__Important__: While this tutorial provides insight into the basics of these libraries,  I recommend digging into the documentation that is available online.*
 
 # ## NumPy
-# 
+#
 # NumPy is the fundamental package for scientific computing with Python. It contains among other things:
-# 
+#
 # * a powerful N-dimensional array object
 # * sophisticated (broadcasting) functions
 # * tools for integrating C/C++ and Fortran code
 # * useful linear algebra, Fourier transform, and random number capabilities
-# 
+#
 # We will focus on the numpy array object.
-# 
+#
 # #### Numpy Array
-# 
-# A numpy array is a grid of values, all of the same type, and is indexed by a tuple of nonnegative integers. The number of dimensions is the rank of the array; the shape of an array is a tuple of integers giving the size of the array along each dimension. 
+#
+# A numpy array is a grid of values, all of the same type, and is indexed by a tuple of nonnegative integers. The number of dimensions is the rank of the array; the shape of an array is a tuple of integers giving the size of the array along each dimension.
 
 # In[ ]:
 
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from scipy import stats
 import numpy as np
 
 
 # In[ ]:
 
 
-### Create a 3x1 numpy array
-a = np.array([1,2,3])
+# Create a 3x1 numpy array
+a = np.array([1, 2, 3])
 
-### Print object type
+# Print object type
 print(type(a))
 
-### Print shape
+# Print shape
 print(a.shape)
 
-### Print some values in a
+# Print some values in a
 print(a[0], a[1], a[2])
 
-### Create a 2x2 numpy array
-b = np.array([[1,2],[3,4]])
+# Create a 2x2 numpy array
+b = np.array([[1, 2], [3, 4]])
 
-### Print shape
+# Print shape
 print(b.shape)
 
-## Print some values in b
-print(b[0,0], b[0,1], b[1,1])
+# Print some values in b
+print(b[0, 0], b[0, 1], b[1, 1])
 
-### Create a 3x2 numpy array
-c = np.array([[1,2],[3,4],[5,6]])
+# Create a 3x2 numpy array
+c = np.array([[1, 2], [3, 4], [5, 6]])
 
-### Print shape
+# Print shape
 print(c.shape)
 
-### Print some values in c
-print(c[0,1], c[1,0], c[2,0], c[2,1])
+# Print some values in c
+print(c[0, 1], c[1, 0], c[2, 0], c[2, 1])
 
 
 # In[ ]:
 
 
-### 2x3 zero array 
-d = np.zeros((2,3))
+# 2x3 zero array
+d = np.zeros((2, 3))
 
 print(d)
 
-### 4x2 array of ones
-e = np.ones((4,2))
+# 4x2 array of ones
+e = np.ones((4, 2))
 
 print(e)
 
-### 2x2 constant array
-f = np.full((2,2), 9)
+# 2x2 constant array
+f = np.full((2, 2), 9)
 
 print(f)
 
-### 3x3 random array
-g = np.random.random((3,3))
+# 3x3 random array
+g = np.random.random((3, 3))
 
 print(g)
 
@@ -99,12 +104,12 @@ print(g)
 # In[ ]:
 
 
-### Create 3x4 array
-h = np.array([[1,2,3,4,], [5,6,7,8], [9,10,11,12]])
+# Create 3x4 array
+h = np.array([[1, 2, 3, 4, ], [5, 6, 7, 8], [9, 10, 11, 12]])
 
 print(h)
 
-### Slice array to make a 2x2 sub-array
+# Slice array to make a 2x2 sub-array
 i = h[:2, 1:3]
 
 print(i)
@@ -113,13 +118,13 @@ print(i)
 # In[ ]:
 
 
-print(h[0,1])
+print(h[0, 1])
 
-### Modify the slice
-i[0,0] = 1738
+# Modify the slice
+i[0, 0] = 1738
 
-### Print to show how modifying the slice also changes the base object
-print(h[0,1])
+# Print to show how modifying the slice also changes the base object
+print(h[0, 1])
 
 
 # #### Datatypes in Arrays
@@ -127,29 +132,29 @@ print(h[0,1])
 # In[ ]:
 
 
-### Integer
+# Integer
 j = np.array([1, 2])
-print(j.dtype)  
+print(j.dtype)
 
 
-### Float
+# Float
 k = np.array([1.0, 2.0])
-print(k.dtype)         
+print(k.dtype)
 
-### Force Data Type
+# Force Data Type
 l = np.array([1.0, 2.0], dtype=np.int64)
 print(l.dtype)
 
 
 # #### Array Math
-# 
+#
 # Basic mathematical functions operate elementwise on arrays, and are available both as operator overloads and as functions in the numpy module:
 
 # In[ ]:
 
 
-x = np.array([[1,2],[3,4]], dtype=np.float64)
-y = np.array([[5,6],[7,8]], dtype=np.float64)
+x = np.array([[1, 2], [3, 4]], dtype=np.float64)
+y = np.array([[5, 6], [7, 8]], dtype=np.float64)
 
 # Elementwise sum; both produce the array
 # [[ 6.0  8.0]
@@ -184,72 +189,66 @@ print(np.sqrt(x))
 # In[ ]:
 
 
-x = np.array([[1,2],[3,4]])
+x = np.array([[1, 2], [3, 4]])
 
-### Compute sum of all elements; prints "10"
+# Compute sum of all elements; prints "10"
 print(np.sum(x))
 
-### Compute sum of each column; prints "[4 6]"
-print(np.sum(x, axis=0)) 
+# Compute sum of each column; prints "[4 6]"
+print(np.sum(x, axis=0))
 
-### Compute sum of each row; prints "[3 7]"
+# Compute sum of each row; prints "[3 7]"
 print(np.sum(x, axis=1))
 
 
 # In[ ]:
 
 
-x = np.array([[1,2],[3,4]])
+x = np.array([[1, 2], [3, 4]])
 
-### Compute mean of all elements; prints "2.5"
+# Compute mean of all elements; prints "2.5"
 print(np.mean(x))
 
-### Compute mean of each column; prints "[2 3]"
-print(np.mean(x, axis=0)) 
+# Compute mean of each column; prints "[2 3]"
+print(np.mean(x, axis=0))
 
-### Compute mean of each row; prints "[1.5 3.5]"
+# Compute mean of each row; prints "[1.5 3.5]"
 print(np.mean(x, axis=1))
 
 
 # ## SciPy
-# 
+#
 # Numpy provides a high-performance multidimensional array and basic tools to compute with and manipulate these arrays. SciPy builds on this, and provides a large number of functions that operate on numpy arrays and are useful for different types of scientific and engineering applications.
-# 
+#
 # For this course, we will primariyl be using the **SciPy.Stats** sub-library.
-# 
+#
 # ### SciPy.Stats
-# 
+#
 # The SciPy.Stats module contains a large number of probability distributions as well as a growing library of statistical functions such as:
-# 
+#
 # * Continuous and Discrete Distributions (i.e Normal, Uniform, Binomial, etc.)
-# 
+#
 # * Descriptive Statistcs
-# 
+#
 # * Statistical Tests (i.e T-Test)
 
 # In[ ]:
 
 
-from scipy import stats
-import numpy as np
+# In[ ]:
+
+
+# Print Normal Random Variables
+print(stats.norm.rvs(size=10))
 
 
 # In[ ]:
 
-
-### Print Normal Random Variables
-print(stats.norm.rvs(size = 10))
-
-
-# In[ ]:
-
-
-from pylab import *
 
 # Create some test data
 dx = .01
-X  = np.arange(-2,2,dx)
-Y  = exp(-X**2)
+X = np.arange(-2, 2, dx)
+Y = exp(-X**2)
 
 # Normalize the data to a proper PDF
 Y /= (dx*Y).sum()
@@ -258,8 +257,8 @@ Y /= (dx*Y).sum()
 CY = np.cumsum(Y*dx)
 
 # Plot both
-plot(X,Y)
-plot(X,CY,'r--')
+plot(X, Y)
+plot(X, CY, 'r--')
 
 show()
 
@@ -267,8 +266,8 @@ show()
 # In[ ]:
 
 
-### Compute the Normal CDF of certain values.
-print(stats.norm.cdf(np.array([1,-1., 0, 1, 3, 4, -2, 6])))
+# Compute the Normal CDF of certain values.
+print(stats.norm.cdf(np.array([1, -1., 0, 1, 3, 4, -2, 6])))
 
 
 # #### Descriptive Statistics
@@ -300,14 +299,10 @@ stats.describe(x)
 # Later in the course, we will discuss distributions and statistical tests such as a T-Test.  SciPy has built in functions for these operations.
 
 # ## MatPlotLib
-# 
+#
 # Matplotlib is a plotting library. In this section give a brief introduction to the matplotlib.pyplot module.
 
 # In[ ]:
-
-
-import numpy as np
-import matplotlib.pyplot as plt
 
 
 # In[ ]:
@@ -345,9 +340,6 @@ plt.show()
 # In[ ]:
 
 
-import numpy as np
-import matplotlib.pyplot as plt
-
 # Compute the x and y coordinates for points on sine and cosine curves
 x = np.arange(0, 3 * np.pi, 0.1)
 y_sin = np.sin(x)
@@ -371,22 +363,19 @@ plt.show()
 
 
 # ## Seaborn
-# 
+#
 # Seaborn is complimentary to Matplotlib and it specifically targets statistical data visualization. But it goes even further than that: Seaborn extends Matplotlib and makes generating visualizations convenient.
-# 
+#
 # While Matplotlib is a robust solution for various problems, Seaborn utilizes more concise paramesters for ease-of-use.
-# 
+#
 # #### Scatterplots
-# 
-# 
+#
+#
 
 # In[ ]:
 
 
 # Import necessary libraries
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
 
 # Store the url string that hosts our .csv file
 url = "Cartwheeldata.csv"
@@ -405,7 +394,7 @@ plt.show()
 
 # Scatterplot arguments
 sns.lmplot(x='Wingspan', y='CWDistance', data=df,
-           fit_reg=False, # No regression line
+           fit_reg=False,  # No regression line
            hue='Gender')   # Color by evolution stage
 
 plt.show()
@@ -425,7 +414,8 @@ plt.show()
 # In[ ]:
 
 
-sns.boxplot(data=df.loc[:, ["Age", "Height", "Wingspan", "CWDistance", "Score"]])
+sns.boxplot(
+    data=df.loc[:, ["Age", "Height", "Wingspan", "CWDistance", "Score"]])
 
 plt.show()
 
@@ -434,7 +424,8 @@ plt.show()
 
 
 # Male Boxplot
-sns.boxplot(data=df.loc[df['Gender'] == 'M', ["Age", "Height", "Wingspan", "CWDistance", "Score"]])
+sns.boxplot(data=df.loc[df['Gender'] == 'M', [
+            "Age", "Height", "Wingspan", "CWDistance", "Score"]])
 
 plt.show()
 
@@ -443,7 +434,8 @@ plt.show()
 
 
 # Female Boxplot
-sns.boxplot(data=df.loc[df['Gender'] == 'F', ["Age", "Height", "Wingspan", "CWDistance", "Score"]])
+sns.boxplot(data=df.loc[df['Gender'] == 'F', [
+            "Age", "Height", "Wingspan", "CWDistance", "Score"]])
 
 plt.show()
 
@@ -484,8 +476,7 @@ plt.show()
 
 # Count Plot (a.k.a. Bar Plot)
 sns.countplot(x='Gender', data=df)
- 
+
 plt.xticks(rotation=-45)
 
 plt.show()
-
