f72c9aa0d4c571777e365e04cd199bc401f34b97
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-11-25 09:38:28 +0530

    Update Tables_Histograms_and_Boxplots_in_Python.py

--- Statistics_with_Python_Specialization/C1_DataViz_Python/Tables_Histograms_and_Boxplots_in_Python.py
+++ Statistics_with_Python_Specialization/C1_DataViz_Python/Tables_Histograms_and_Boxplots_in_Python.py
@@ -3,24 +3,24 @@
 
 # ### Visualizing Data in Python
 # #### Tables, Histograms, Boxplots, and Slicing for Statistics
-# 
+#
 # When working with a new dataset, one of the most useful things to do is to begin to visualize the data. By using tables, histograms, box plots, and other visual tools, we can get a better idea of what the data may be trying to tell us, and we can gain insights into the data that we may have not discovered otherwise.
-# 
+#
 # Today, we will be going over how to perform some basic visualisations in Python, and, most importantly, we will learn how to begin exploring data from a graphical perspective.
 
 # In[ ]:
 
 
 # We first need to import the packages that we will be using
-import seaborn as sns # For plotting
-import matplotlib.pyplot as plt # For showing plots
+import seaborn as sns  # For plotting
+import matplotlib.pyplot as plt  # For showing plots
 
 # Load in the data set
 tips_data = sns.load_dataset("tips")
 
 
 # #### Visualizing the Data - Tables
-# When you begin working with a new data set,  it is often best to print out the first few rows before you begin other analysis. This will show you what kind of data is in the dataset, what data types you are working with, and will serve as a reference for the other plots that we are about to make. 
+# When you begin working with a new data set,  it is often best to print out the first few rows before you begin other analysis. This will show you what kind of data is in the dataset, what data types you are working with, and will serve as a reference for the other plots that we are about to make.
 
 # In[ ]:
 
@@ -30,7 +30,7 @@ tips_data.head()
 
 
 # #### Describing Data
-# Summary statistics, which include things like the mean, min, and max of the data, can be useful to get a feel for how large some of the variables are and what variables may be the most important. 
+# Summary statistics, which include things like the mean, min, and max of the data, can be useful to get a feel for how large some of the variables are and what variables may be the most important.
 
 # In[ ]:
 
@@ -40,14 +40,15 @@ tips_data.describe()
 
 
 # #### Creating a Histogram
-# 
+#
 # After we have a general 'feel' for the data, it is often good to get a feel for the shape of the distribution of the data.
 
 # In[ ]:
 
 
 # Plot a histogram of the total bill
-sns.distplot(tips_data["total_bill"], kde = False).set_title("Histogram of Total Bill")
+sns.distplot(tips_data["total_bill"], kde=False).set_title(
+    "Histogram of Total Bill")
 plt.show()
 
 
@@ -55,7 +56,7 @@ plt.show()
 
 
 # Plot a histogram of the Tips only
-sns.distplot(tips_data["tip"], kde = False).set_title("Histogram of Total Tip")
+sns.distplot(tips_data["tip"], kde=False).set_title("Histogram of Total Tip")
 plt.show()
 
 
@@ -63,13 +64,14 @@ plt.show()
 
 
 # Plot a histogram of both the total bill and the tips'
-sns.distplot(tips_data["total_bill"], kde = False)
-sns.distplot(tips_data["tip"], kde = False).set_title("Histogram of Both Tip Size and Total Bill")
+sns.distplot(tips_data["total_bill"], kde=False)
+sns.distplot(tips_data["tip"], kde=False).set_title(
+    "Histogram of Both Tip Size and Total Bill")
 plt.show()
 
 
 # #### Creating a Boxplot
-# 
+#
 # Boxplots do not show the shape of the distribution, but they can give us a better idea about the center and spread of the distribution as well as any potential outliers that may exist. Boxplots and Histograms often complement each other and help an analyst get more information about the data
 
 # In[ ]:
@@ -98,14 +100,14 @@ plt.show()
 
 
 # #### Creating Histograms and Boxplots Plotted by Groups
-# 
+#
 # While looking at a single variable is interesting, it is often useful to see how a variable changes in response to another. Using graphs, we can see if there is a difference between the tipping amounts of smokers vs. non-smokers, if tipping varies according to the time of the day, or we can explore other trends in the data as well.
 
 # In[ ]:
 
 
 # Create a boxplot and histogram of the tips grouped by smoking status
-sns.boxplot(x = tips_data["tip"], y = tips_data["smoker"])
+sns.boxplot(x=tips_data["tip"], y=tips_data["smoker"])
 plt.show()
 
 
@@ -113,9 +115,9 @@ plt.show()
 
 
 # Create a boxplot and histogram of the tips grouped by time of day
-sns.boxplot(x = tips_data["tip"], y = tips_data["time"])
+sns.boxplot(x=tips_data["tip"], y=tips_data["time"])
 
-g = sns.FacetGrid(tips_data, row = "time")
+g = sns.FacetGrid(tips_data, row="time")
 g = g.map(plt.hist, "tip")
 plt.show()
 
@@ -124,9 +126,8 @@ plt.show()
 
 
 # Create a boxplot and histogram of the tips grouped by the day
-sns.boxplot(x = tips_data["tip"], y = tips_data["day"])
+sns.boxplot(x=tips_data["tip"], y=tips_data["day"])
 
-g = sns.FacetGrid(tips_data, row = "day")
+g = sns.FacetGrid(tips_data, row="day")
 g = g.map(plt.hist, "tip")
 plt.show()
-
