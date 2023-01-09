ff37fdb927de92a18ba7ddfc72d60f2db05f4634
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-01-09 13:32:13 +0530

    cost funvtoin

--- Mathematics_for_Machine_Learning_Specialization/C2_Multivariate_calculus/W4_BNN.py
+++ Mathematics_for_Machine_Learning_Specialization/C2_Multivariate_calculus/W4_BNN.py
@@ -1,4 +1,5 @@
 # First we set the state of the network
+import numpy as np
 σ = np.tanh
 w1 = 1.3
 b1 = -0.1
@@ -10,7 +11,14 @@ def a1(a0):
     z = w1 * a0 + b1
     return σ(z)
 
+# Set y here
+
+
+y = 0
 
 # Experiment with different values of x below.
 x = 0
 a1(x)
+
+# Cost function
+c_k = (a1(x) - y) ^ 2
