e6b8fdf0bc93cd8bffd03cc954c3879aca5da8d4
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-01-27 09:18:28 +0530

    Create mean.py

---
+++ Mathematics_for_Machine_Learning_Specialization/C3_PCA/W1/mean.py
@@ -0,0 +1,16 @@
+# %%
+import numpy as np
+
+a1 = np.array([1, 4, 7])
+a2 = np.array([2, 5, 8])
+a3 = np.array([3, 6, 9])
+(a1+a2+a3)/3
+
+# %%
+a1 = np.array([1, 2, 3])
+a2 = np.array([3, 4, 5])
+a3 = np.array([5, 3, 1])
+
+(a1*2+a2*2+a3*2)/3
+
+# %%
