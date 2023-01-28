05cc175291d31f261ce75fe9d2b3c61ed072de5a
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-01-28 13:10:21 +0530

    Update normalize.py

---
+++ Mathematics_for_Machine_Learning_Specialization/C3_PCA/W1/normalize.py
@@ -0,0 +1,25 @@
+def normalize(x):
+    """
+    Normalize all the columns of x
+
+    Arguments:
+    x: A two dimensional Numpy array
+
+    Returns:
+    c: The normalized version of x
+    """
+
+    # BEGIN SOLUTION
+
+    # Calculate the mean of all columns
+    mean = None
+
+    # Calculate the standard deviation all columns
+    sigma = None
+
+    # Compute the final answer
+    c = None
+
+    # END SOLUTION
+
+    return c
