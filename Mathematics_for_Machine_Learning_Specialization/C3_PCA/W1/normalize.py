f526bd15b01956c5013716fb372655b1b49c2c7c
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-01-28 13:37:48 +0530

    Update normalize.py

--- Mathematics_for_Machine_Learning_Specialization/C3_PCA/W1/normalize.py
+++ Mathematics_for_Machine_Learning_Specialization/C3_PCA/W1/normalize.py
@@ -1,3 +1,8 @@
+
+# %%
+import numpy as np
+
+
 def normalize(x):
     """
     Normalize all the columns of x
@@ -12,14 +17,34 @@ def normalize(x):
     # BEGIN SOLUTION
 
     # Calculate the mean of all columns
-    mean = None
+    mean = np.mean(x, axis=0, keepdims=True)
 
     # Calculate the standard deviation all columns
-    sigma = None
+    sigma = np.std(x, axis=0, keepdims=True)
 
     # Compute the final answer
-    c = None
+    c = (x - mean) / sigma
 
     # END SOLUTION
 
     return c
+
+
+# %%
+x = np.array([[1, 4],
+              [3, 2]])
+expected = np.array([[-1, 1],
+                     [1, -1]])
+np.testing.assert_allclose(normalize(x), expected, rtol=1e-5)
+
+x = np.array([[324.33, 136.11, 239.38, 237.17],
+              [123.43, 325.24, 243.52, 745.25],
+              [856.36, 903.02, 430.25, 531.35]])
+expected = np.array([[-0.35694152, -0.97689929, -0.73023324, -1.28391946],
+                     [-1.00662188, -0.39712973, -0.68372539,  1.1554411],
+                     [1.3635634,  1.37402902,  1.41395863,  0.12847837]])
+np.testing.assert_allclose(normalize(x), expected, rtol=1e-5)
+
+print("All tests passed!")
+
+# %%
