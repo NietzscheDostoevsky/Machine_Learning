9015d29d04f844d84c138d7daf0ec512b85bdd84
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-01-30 15:35:09 +0530

    Update length.py

---
+++ Mathematics_for_Machine_Learning_Specialization/W2/length.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def length(x):
+    """Compute the length of a vector"""
+    length_x = 0
+    for i in range(x.shape[0]):
+        length_x = length_x + (x[i] * x[i])
+
+    length_x = np.sqrt(length_x)
+
+    return length_x
+
+
+print(length(np.array([1, 0])))
